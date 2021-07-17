ecc593d8f7272db15c8476bdd84477abbb1caa7c
Vitor Rubim <vitorrubim1@hotmail.com>
2021-07-17 15:20:23 -0300

    feat: adicionando hook de pokemon

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import { ChakraProvider } from '@chakra-ui/react';
 
 import Routes from './routes';
 import theme from './styles/theme';
+import AppProvider from './hooks';
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <Routes />
+      <AppProvider>
+        <Routes />
+      </AppProvider>
     </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root'),
